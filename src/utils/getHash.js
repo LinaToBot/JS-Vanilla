b8954ac8feec743fb50507b7281ee4f812563eac
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-08-03 21:02:19 -0600

    routes added

--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -1,4 +1,7 @@
-const getHash = () =>
-  location.hash.slice(1).toLocaleLowerCase().split("/"[1]) || "/";
+const getHash = () => {
+  const hash = location.hash.slice(1).toLocaleLowerCase(); // Remover el primer carácter (#) y convertir a minúsculas
+  return hash.split("/")[1] || "/"; // Para verificar que el hash se obtiene correctamente
+};
 
 export default getHash;
+// location.hash.slice(1).toLocaleLowerCase().split("/"[1]) || "/"
